f1464a776f8ceeb2f1f25ae129c57513f258c4d6
dailiwei <489388296@qq.com>
2016-07-14 09:23:31 +0800

    2016-7-14

---
+++ client/Sandcastle/gallery/gallery-index.js
@@ -0,0 +1,16 @@
+// This file is automatically rebuilt by the Cesium build process.
+var gallery_demos = [
+  {
+    "name": "Hello World",
+    "date": 1456847845601,
+    "img": "Hello World.jpg",
+    "path":"simple/demo_/demoView"
+  },
+  {
+    "name": "FloatOneLayout",
+    "date": 1456847845609,
+    "img": "Hello World.jpg",
+    "path":"base/layout/FloatOneLayout"
+  }
+
+];
